8b748ffae7cfa4a9c4e72bc123712def4313c0eb
marcu <mhm3884@rit.edu>
2024-11-21 14:23:59 -0500

    Changed index file and started header

---
+++ Assets/inc/header.inc.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Group 6 ISTE-240 Project 1</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+<body>
+    <header>
+        <nav class="topNav">
+            <a href="index.pgp" class="selectedNav">Home</a>
+            <a href="principles.pgp">Principles</a>
+            <a href="accessability.php">Accessability</a>
+            <a href="about.php">About</a>
+        </nav>
+    </header>
