a3ff0acd725dd2627defce162889503b101c3fe5
Jay Freeb <liege@liege.dev>
2024-12-01 16:23:13 -0500

    completed footer

--- Assets/inc/footer.inc.php
+++ Assets/inc/footer.inc.php
@@ -1,10 +1,26 @@
         <footer>
-            <a href="about.php">About</a>
-            <a href="accessibility.php">Accessability</a>
-            <a href="cantact.php">Contact</a>
-            <a href="#">Other Thing</a>
-            
-        </footer>    
-    
+            <link rel="stylesheet" type="text/css" href="./Assets/css/footer.css">
+            <div class="footer-container">
+                <div class="footer-links">
+                    <ul>
+                        <li>Principles</li>
+                        <li><a href="./contrast">Contrast</a></li>
+                        <li><a href="./repetition">Repetition</a></li>
+                        <li><a href="./alignment">Alignment</a></li>
+                        <li><a href="./proximity">Proximity</a></li>
+                    </ul>
+                    <ul>
+                        <li>Other</li>
+                        <li><a href="./index.php">Home</a></li>
+                        <li><a href="./about.php">About</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>CRAP Principles</h2>
+                    <b>Web Dev II</b>
+                    <p>Name, Name, Name</p>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
