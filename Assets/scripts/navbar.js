82be19d1b6645e17e2a7263b92f2569b38415ffd
Jay Freeb <liege@liege.dev>
2024-12-02 00:23:32 -0500

    fixed broken navbar resizing

--- Assets/scripts/navbar.js
+++ Assets/scripts/navbar.js
@@ -17,3 +17,27 @@ function closeNav() {
     hamburger.style.display = "flex";
     x.style.display = "none";
 }
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 800) {
+        let nav = document.getElementById("navbar-list");
+        let hamburger = document.getElementById("hamburger");
+        let x = document.getElementById("x");
+
+        nav.style.display = "flex";
+        hamburger.style.display = "none";
+        x.style.display = "none";
+    }
+});
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth < 800) {
+        let nav = document.getElementById("navbar-list");
+        let hamburger = document.getElementById("hamburger");
+        let x = document.getElementById("x");
+
+        nav.style.display = "none";
+        hamburger.style.display = "flex";
+        x.style.display = "none";
+    }
+});
