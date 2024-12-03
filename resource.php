331f30558596fe82efb78893b17adcc2d15eb34b
John <john.ettipio@rit.edu>
2024-12-02 19:33:59 -0500

    Added links to resource page

--- resource.php
+++ resource.php
@@ -2,6 +2,63 @@
     require_once("assets/inc/header.inc.php");
 ?>
     <main>
+        <ul>
+            <li><li><a class="refLink" href="https://diegopiovesan.wordpress.com/wp-content/uploads/2010/07/livro_-_the_non-designers_desi.pdf">
+                The Non-Designer's Design Book
+            </a></li>
+            <li><a class="refLink" href="https://attentioninsight.com/crap-design-principles/">
+               Attention Insight - 4 CRAP Design Principles For A Better UX 
+            </a></li>
+            <li><a class="refLink" href="https://lewisu.edu/writingcenter/pdf/crap-resource-revised-pub.pdf">
+                Lewis University Writing Center
+            </a></li>
+            <li><a class="refLink" href="https://clueify.com/blog/crap-design-principles/">
+                Clueify - Crap Design Principles
+            </a></li>
+            <li><a class="refLink" href="https://vwo.com/blog/crap-design-principles/">
+                VWO - How to Use C.R.A.P. Design links with Principles for Better UX?
+            </a></li>
+            <li><a class="refLink" href="https://www.artworkflowhq.com/resources/7-worst-color-combinations-you-need-to-avoid">
+                Art Work Flow - 7 Worst Color Combinations You Need To Avoid
+            </a></li>
+            <li><a class="refLink" href="https://www.wix.com/studio/blog/interaction-design">
+                Wix - Interaction Design
+            </a></li>
+            <li><a class="refLink" href="https://baymard.com/blog/line-length-readability">
+                Baymard Institute - Readability: The Optimal Line Length
+            </a></li>
+            <li><a class="refLink" href="https://adasitecompliance.com/website-accessibility-for-deaf-and-hard-of-hearing/">
+                
+            </a></li>
+            <li><a class="refLink" href="https://coolors.co/241023-6b0504-a3320b-d5e68d-47a025">
+                
+            </a></li>
+            <li><a class="refLink" href="https://www.boia.org/blog/the-5-most-common-website-accessibility-issues-and-how-to-fix-them">
+                
+            </a></li>
+            <li><a class="refLink" href="https://webaim.org/projects/million">
+                
+            </a></li>
+            <li><a class="refLink" href="https://thewebsitearchitect.com/does-center-aligned-text-matter-for-accessibility/">
+                
+            </a></li>
+            <li><a class="refLink" href="https://www.fbi.gov/how-we-can-help-you/scams-and-safety/common-frauds-and-scams/spoofing-and-phishing">
+                
+            </a></li>
+            <li><a class="refLink" href="https://en.wikipedia.org/wiki/Contrast_effect">
+                
+            </a></li>
+            <li><a class="refLink" href="https://www.crazyegg.com/blog/why-users-leave-a-website/">
+                
+            </a></li>
+            <li><a class="refLink" href="https://mercuryworks.com/blog/are-you-designing-with-c-r-a-p-2">
+                
+            </a></li>
+            <li><a class="refLink" href="https://www.canva.com/learn/contrasting-colors/">
+                
+            </a></li>
+        </ul>
+        
         
     </main>
 <?php
