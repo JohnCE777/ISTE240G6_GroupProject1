fd658b2287ed959d57815f89b50ec2c0ebe757d3
Jay Freeb <liege@liege.dev>
2024-12-03 14:04:00 -0500

    added button for quizes on principle pages

--- contrast.php
+++ contrast.php
@@ -61,6 +61,11 @@
                 </div>
             </div>
         </section>
+        <hr>
+        <div class="quiz-offer">
+            <h2>Ready to test your knowledge?</h2>     
+            <button class="quiz-offer-button"><a href="./contrastquiz.php"></a>Take the Quiz</a></button>      
+        </div>
     </main>
 <?php
     require_once("assets/inc/footer.inc.php");
