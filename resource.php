423210a4fd31a8475417e3bed8923ed63c48e30c
John <john.ettipio@rit.edu>
2024-12-03 11:48:05 -0500

    Finished adding resource links

--- resource.php
+++ resource.php
@@ -28,34 +28,34 @@
                 Baymard Institute - Readability: The Optimal Line Length
             </a></li>
             <li><a class="refLink" href="https://adasitecompliance.com/website-accessibility-for-deaf-and-hard-of-hearing/">
-                
+                ADA Site Compliance - Website Accessibility for Deaf and Hard of Hearing
             </a></li>
             <li><a class="refLink" href="https://coolors.co/241023-6b0504-a3320b-d5e68d-47a025">
-                
+                Coolors - Color Palette Selector
             </a></li>
             <li><a class="refLink" href="https://www.boia.org/blog/the-5-most-common-website-accessibility-issues-and-how-to-fix-them">
-                
+                Bureau of Internet Accessibility - The 5 Most Common Website Accessibility Issues (And How To Fix Them)
             </a></li>
             <li><a class="refLink" href="https://webaim.org/projects/million">
-                
+                Web Accessibility in Mind - The WebAIM Million
             </a></li>
             <li><a class="refLink" href="https://thewebsitearchitect.com/does-center-aligned-text-matter-for-accessibility/">
-                
+                The Website Architect - Does Text Alignment Matter for Accessibility and Usability?
             </a></li>
             <li><a class="refLink" href="https://www.fbi.gov/how-we-can-help-you/scams-and-safety/common-frauds-and-scams/spoofing-and-phishing">
-                
+                FBI - Spoofing and Phishing
             </a></li>
             <li><a class="refLink" href="https://en.wikipedia.org/wiki/Contrast_effect">
-                
+                Wikipedia - Contrast Effect
             </a></li>
             <li><a class="refLink" href="https://www.crazyegg.com/blog/why-users-leave-a-website/">
-                
+                Crazy Egg - Why Users Leave a Website
             </a></li>
             <li><a class="refLink" href="https://mercuryworks.com/blog/are-you-designing-with-c-r-a-p-2">
-                
+                MercuryWorks - Four Rules at the Foundation of Design
             </a></li>
             <li><a class="refLink" href="https://www.canva.com/learn/contrasting-colors/">
-                
+                Canva - Designing with contrast: 20 tips from a designer
             </a></li>
         </ul>
         
