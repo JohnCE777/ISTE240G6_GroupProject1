ce873d5e78e43468003298c5c19dcd860a613c0e
marcu <mhm3884@rit.edu>
2024-12-03 13:47:32 -0500

    changed form

--- Assets/inc/contactForm.inc.php
+++ Assets/inc/contactForm.inc.php
@@ -1,13 +1,17 @@
-<form name="contactForm" action="contact.php" onsubmit="return validateForm();" method="POST">
-    <label for="email">Email:</label>
-    <input type="text" id="email" name="email">
-    <br>
-    <label for="name">Name:</Label>
-    <input type="text" id="name" name="name">
-    <br>
-    <label for="phone">phone number: </label>
-    <br>
-    <input type="tel" id="phone" name="phone">
-    <br>
-    <input type="submit" value="Submit">
-</form>
+<div class="contactContainer">
+    <form name="contactForm" action="contact.php" onsubmit="return validateForm();" method="POST">
+        <label for="email">Email:</label>
+        <br>
+        <input type="text" id="email" name="email">
+        <br>
+        <label for="name">Name:</Label>
+        <br>
+        <input type="text" id="name" name="name">
+        <br>
+        <label for="phone">phone number: </label>
+        <br>
+        <input type="tel" id="phone" name="phone">
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</div>
