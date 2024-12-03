2155cd4d1014e344f60a9b5951c7fa6aaf225a35
Jay Freeb <liege@liege.dev>
2024-12-03 13:36:16 -0500

    quiz styles updates

--- Assets/inc/accessibilityQuiz.inc.php
+++ Assets/inc/accessibilityQuiz.inc.php
@@ -1,99 +1,109 @@
 <div id="quiz">
-    <fieldset>
+    <div class="quiz-question">
         <legend>Why is accessibility important when creating websites?</legend>
-        <div>
-            <input type="radio" id="answer1" name="question1" value="incorrect">
-            <label for="answer1">To make the site look more professional</label>
-        </div>
-        <div>
-            <input type="radio" id="answer2" name="question1" value="correct">
-            <label for="answer2">To ensure no users are excluded and that the site is usable by everyone</label>
-        </div>
-        <div>
-            <input type="radio" id="answer3" name="question1" value="incorrect">
-            <label for="answer3">To make the website load faster</label>
-           </div>
-        <div>
-            <input type="radio" id="answer4" name="question1" value="incorrect">
-            <label for="answer4">To increase the website’s popularity</label>
-        </div>
-    </fieldset>
-    <fieldset>
+        <ul>
+            <li>
+                <input type="radio" id="answer1" name="question1" value="incorrect">
+                <label for="answer1">To make the site look more professional</label>
+            </li>
+            <li>
+                <input type="radio" id="answer2" name="question1" value="correct">
+                <label for="answer2">To ensure no users are excluded and that the site is usable by everyone</label>
+            </li>
+            <li>
+                <input type="radio" id="answer3" name="question1" value="incorrect">
+                <label for="answer3">To make the website load faster</label>
+            </li>
+            <li>
+                <input type="radio" id="answer4" name="question1" value="incorrect">
+                <label for="answer4">To increase the website’s popularity</label>
+            </li>
+        </ul>
+    </div>
+    <div class="quiz-question">
         <legend>What is one common mistake developers make regarding text color contrast?</legend>
-        <div>
-            <input type="radio" id="answer5" name="question2" value="incorrect">
-            <label for="answer5">Using too many colors</label>
-        </div>
-        <div>
-            <input type="radio" id="answer6" name="question2" value="correct">
-            <label for="answer6">Using poor text contrast, making the text hard to read, especially for vision-impaired users</label>
-        </div>
-        <div>
-            <input type="radio" id="answer7" name="question2" value="incorrect">
-            <label for="answer7">Making text too small to read</label>
-        </div>
-        <div>
-            <input type="radio" id="answer8" name="question2" value="incorrect">
-            <label for="answer8">Using the same color for the background and text</label>
-        </div>
-    </fieldset>
-    <fieldset>
+        <ul>
+            <li>
+                <input type="radio" id="answer5" name="question2" value="incorrect">
+                <label for="answer5">Using too many colors</label>
+            </li>
+            <li>
+                <input type="radio" id="answer6" name="question2" value="correct">
+                <label for="answer6">Using poor text contrast, making the text hard to read, especially for vision-impaired users</label>
+            </li>
+            <li>
+                <input type="radio" id="answer7" name="question2" value="incorrect">
+                <label for="answer7">Making text too small to read</label>
+            </li>
+            <li>
+                <input type="radio" id="answer8" name="question2" value="incorrect">
+                <label for="answer8">Using the same color for the background and text</label>
+            </li>
+        </ul>
+    </div>
+    <div class="quiz-question">
         <legend>What is the purpose of alternative text (alt text) on a website?</legend>
-        <div>
-            <input type="radio" id="answer9" name="question3" value="incorrect">
-            <label for="answer9">To display an image when the website is loaded correctly</label>
-        </div>
-        <div>
-            <input type="radio" id="answer10" name="question3" value="correct">
-            <label for="answer10">To describe the image for users who can't see it, especially for screen reader users</label>
-        </div>
-        <div>
-            <input type="radio" id="answer11" name="question3" value="incorrect">
-            <label for="answer11">To make the website look more colorful</label>
-        </div>
-        <div>
-            <input type="radio" id="answer12" name="question3" value="incorrect">
-            <label for="answer12">To reduce the loading time of the website</label>
-        </div>
-    </fieldset>
-    <fieldset>
+        <ul>
+            <li>
+                <input type="radio" id="answer9" name="question3" value="incorrect">
+                <label for="answer9">To display an image when the website is loaded correctly</label>
+            </li>
+            <li>
+                <input type="radio" id="answer10" name="question3" value="correct">
+                <label for="answer10">To describe the image for users who can't see it, especially for screen reader users</label>
+            </li>
+            <li>
+                <input type="radio" id="answer11" name="question3" value="incorrect">
+                <label for="answer11">To make the website look more colorful</label>
+            </li>
+            <li>
+                <input type="radio" id="answer12" name="question3" value="incorrect">
+                <label for="answer12">To reduce the loading time of the website</label>
+            </li>
+        </ul>
+    </div>
+    <div class="quiz-question">
         <legend>Why is it important to include subtitles or captions in videos for users who are Deaf/deaf or hard of hearing?</legend>
-        <div>
-            <input type="radio" id="answer13" name="question4" value="incorrect">
-            <label for="answer13">To help users who don’t understand the language of the video</label>
-        </div>
-        <div>
-            <input type="radio" id="answer14" name="question4" value="correct">
-            <label for="answer14">To describe non-spoken sounds and make video content accessible2</label>
-        </div>
-        <div>
-            <input type="radio" id="answer15" name="question4" value="incorrect">
-            <label for="answer15">To increase the entertainment value of the video</label>
-        </div>
-        <div>
-            <input type="radio" id="answer16" name="question4" value="incorrect">
-            <label for="answer16">To make the video shorter and more concise</label>
-            </div>
-    </fieldset>
-    <fieldset>
+        <ul>
+            <li>
+                <input type="radio" id="answer13" name="question4" value="incorrect">
+                <label for="answer13">To help users who don’t understand the language of the video</label>
+            </li>
+            <li>
+                <input type="radio" id="answer14" name="question4" value="correct">
+                <label for="answer14">To describe non-spoken sounds and make video content accessible2</label>
+            </li>
+            <li>
+                <input type="radio" id="answer15" name="question4" value="incorrect">
+                <label for="answer15">To increase the entertainment value of the video</label>
+            </li>
+            <li>
+                <input type="radio" id="answer16" name="question4" value="incorrect">
+                <label for="answer16">To make the video shorter and more concise</label>
+            </li>
+        </ul>
+    </div>
+    <div class="quiz-question">
         <legend>What is the difference between Deaf and deaf</legend>
-        <div>
-            <input type="radio" id="answer17" name="question5" value="incorrect">
-            <label for="answer17">Using complicated language to challenge users</label>
-        </div>
-        <div>
-            <input type="radio" id="answer18" name="question5" value="correct">
-            <label for="answer18">Avoiding jargon and using simple sentence structures to ensure all audiences understand the content</label>
-        </div>
-        <div>
-            <input type="radio" id="answer19" name="question5" value="incorrect">
-            <label for="answer19">Using a second language in the content</label>
-        </div>
-        <div>
-            <input type="radio" id="answer20" name="question5" value="incorrect">
-            <label for="answer20">Writing content only for English speakers</label>
-        </div>
-    </fieldset>
+        <ul>
+            <li>
+                <input type="radio" id="answer17" name="question5" value="incorrect">
+                <label for="answer17">Using complicated language to challenge users</label>
+            </li>
+            <li>
+                <input type="radio" id="answer18" name="question5" value="correct">
+                <label for="answer18">Avoiding jargon and using simple sentence structures to ensure all audiences understand the content</label>
+            </li>
+            <li>
+                <input type="radio" id="answer19" name="question5" value="incorrect">
+                <label for="answer19">Using a second language in the content</label>
+            </l>
+            <li>
+                <input type="radio" id="answer20" name="question5" value="incorrect">
+                <label for="answer20">Writing content only for English speakers</label>
+            </li>
+        </ul>
+    </div>
     <div>
     	<button onclick="validateQuiz()">submit</button>
         <h3 id="score">score:</h3>
