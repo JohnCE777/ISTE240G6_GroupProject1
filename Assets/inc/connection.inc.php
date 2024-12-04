be7ef163b5fc88b8bc43ccc2cab4b5cebd9c2ef4
Jay Freeb <liege@liege.dev>
2024-12-03 22:36:59 -0500

    styles for contact page

--- Assets/inc/connection.inc.php
+++ Assets/inc/connection.inc.php
@@ -1,10 +1,10 @@
 <?php
-    $username = "iste240t11";
-    $password = "Savageness4+leprechaun";
-    $hostname = "localhost";
-    $databaseName = "iste240t11";
+    // $username = "iste240t11";
+    // $password = "Savageness4+leprechaun";
+    // $hostname = "localhost";
+    // $databaseName = "iste240t11";
 
-    $mysqli = new mysqli($hostname, $username, $password, $databaseName);
+    // $mysqli = new mysqli($hostname, $username, $password, $databaseName);
 
-    //$mysqli = new mysqli("localhost", "root", "Hillside@808", "sld7188");
+    $mysqli = new mysqli("localhost", "root", "Hillside@808", "sld7188");
 ?>
--- Assets/inc/contactForm.inc.php
+++ Assets/inc/contactForm.inc.php
@@ -1,17 +1,11 @@
 <div class="contactContainer">
     <form name="contactForm" action="contact.php" onsubmit="return validateForm();" method="POST">
-        <label for="email">Email:</label>
-        <br>
+        <label for="email">Email</label>
         <input type="text" id="email" name="email">
-        <br>
-        <label for="name">Name:</Label>
-        <br>
+        <label for="name">Name</Label>
         <input type="text" id="name" name="name">
-        <br>
-        <label for="phone">phone number: </label>
-        <br>
+        <label for="phone">Phone number</label>
         <input type="tel" id="phone" name="phone">
-        <br>
-        <input type="submit" value="Submit">
+        <input type="submit" value="Submit" id="contact-submit">
     </form>
 </div>
